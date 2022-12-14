43fc5b6e9d057979615064ec61eac5895b7602f6
RicardoSo <rsotaquira@gmail.com>
2022-12-14 07:48:09 -0500

    improvements routes API

--- server/server.js
+++ server/server.js
@@ -2,5 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.listen(5005, () => console.log('SERVIDOR LEVANTADO'));
 app.get('/productos', (req, res) => res.send('<h1>TESTING FIRST ENDPOINT</h1>'));
+
+app.listen(5005, () => console.log('SERVIDOR LEVANTADO'));
