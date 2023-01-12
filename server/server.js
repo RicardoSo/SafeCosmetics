d100bf4035eeb7df1b1b2afcf38b458c2f348dba
RicardoSo <rsotaquira@gmail.com>
2023-01-12 11:13:14 -0500

    first end point connected to mongo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/productos', (req, res) => res.send('<h1>TESTING FIRST ENDPOINT</h1>'));
+// DB connection
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost/SafeCosmetics')
+    .then(() => console.log('CONECTADO A BBDD'));
+
+
+// Model
+const Coaster = require('./Models/Coaster.model');
+
+//Routing
+app.get('/productos', (req, res) => {
+
+    Coaster
+        .find()
+        .then(allCoasters => res.json(allCoasters))
+});
 
 app.listen(5005, () => console.log('SERVIDOR LEVANTADO'));
