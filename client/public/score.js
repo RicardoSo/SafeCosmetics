ac75d240c9bf6dbe01b0d60c93af1312dcec6453
RicardoSo <rsotaquira@gmail.com>
2023-01-31 09:10:24 -0500

    refactor scoreAdd function

--- client/public/score.js
+++ client/public/score.js
@@ -1,11 +1,7 @@
 let homeScore = 0
-let homeEl = document.getElementById("home-el")
-let homeButton1 = document.getElementById("home1-btn")
-let homeButton2 = document.getElementById("home2-btn")
-let homeButton3 = document.getElementById("home3-btn")
 
 function scoreView() {
-    homeEl.textContent = homeScore
+    document.getElementById("home-el").textContent = homeScore
 }
 
 function scoreReset() {
@@ -16,18 +12,6 @@ function scoreReset() {
 function scoreAdd(increment) {
     homeScore += increment
     scoreView()
-}
-
-function homeAdd1() {
-    scoreAdd(1)
-}
-
-function homeAdd2() {
-    scoreAdd(2)
-}
-
-function homeAdd3() {
-    scoreAdd(3)
 }
 
 scoreReset()
