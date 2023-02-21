c060195bc7df26ddd189b8738dc808e16f849ffe
Ricardo Sotaquirá <rsotaquira@gmail.com>
2023-02-21 07:21:31 -0500

    arrow function handle

--- board/src/App.js
+++ board/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
 
   return (
     <button
@@ -16,18 +16,18 @@ export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = 'O';
+    nextSquares[i] = 'X';
     setSquares(nextSquares);
   }
 
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
         <Square value={squares[3]} />
