edc2522430e25cf8ec5bbe6d1d19dfbcfa169721
Ricardo Sotaquirá <rsotaquira@gmail.com>
2023-06-14 11:10:26 -0500

    improve requestListener

--- server/test.js
+++ server/test.js
@@ -1,8 +1,9 @@
+// pruebas de node js
 const http = require('http');
 
-const requestListener = function (req, res) {
+const requestListener = (req, res) => {
     res.writeHead(200);
-    res.end('Fin de transmision');
+    res.end('Fin de la trama');
 }
 
 const server = http.createServer(requestListener);
