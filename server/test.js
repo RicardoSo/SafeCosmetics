fd2cf0d19add179873386c95b52b8f267a348702
Ricardo Sotaquirá <rsotaquira@gmail.com>
2023-06-15 10:39:44 -0500

    implementing with 2 sequential promises

--- server/test.js
+++ server/test.js
@@ -1,9 +1,24 @@
-// pruebas de node js
-const http = require('http');
+//Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
+let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Long Promise resolved")
+    },6000)})
 
-const requestListener = (req, res) => {
-    res.writeHead(200);
-    res.end('Fin de la trama');
-}
+// Promesa corta
+const shortPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Short Promise resolved")
+    },3000)})
 
-const server = http.createServer(requestListener);
+//Console log before calling the promise
+console.log("Before calling long promise");
+//Call the promise and wait for it to be resolved and then print a message.
+myPromise.then((successMessage) => {
+    console.log("From Callback " + successMessage);
+  })
+//Console log after calling the promise
+  console.log("After calling long promise");
+// Call the short promise
+shortPromise.then( successMessage => {
+    console.log(`From Callback Short - ${successMessage}`);
+})
