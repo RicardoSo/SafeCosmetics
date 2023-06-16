ff41664046e05bda181038cebedca42fe3e7a2f1
Ricardo Sotaquirá <rsotaquira@gmail.com>
2023-06-16 07:28:34 -0500

    including try catch to server method

--- server/test.js
+++ server/test.js
@@ -1,24 +1,14 @@
-//Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
-let myPromise = new Promise((resolve,reject) => {
-    setTimeout(() => {
-      resolve("Long Promise resolved")
-    },6000)})
+const express = require('express');
+const app = express();
 
-// Promesa corta
-const shortPromise = new Promise((resolve,reject) => {
-    setTimeout(() => {
-      resolve("Short Promise resolved")
-    },3000)})
+function metodo(par1, par2) {
+  try {
+    return par1 > par2
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//Console log before calling the promise
-console.log("Before calling long promise");
-//Call the promise and wait for it to be resolved and then print a message.
-myPromise.then((successMessage) => {
-    console.log("From Callback " + successMessage);
-  })
-//Console log after calling the promise
-  console.log("After calling long promise");
-// Call the short promise
-shortPromise.then( successMessage => {
-    console.log(`From Callback Short - ${successMessage}`);
-})
+exports.metodo = metodo;
+
+console.log(metodo(6,2));
