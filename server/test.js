9ab1a6a4af53daa81045dbe989604b8df147fad2
Ricardo Sotaquirá <rsotaquira@gmail.com>
2023-06-21 09:34:19 -0500

    async programming

--- server/test.js
+++ server/test.js
@@ -1,12 +1,31 @@
+/*
+In the following code we try to get list of all entries from remote url and then based on that make request about each of the 
+category starting with 'A'. Finally print the API counts of the category. We are using axios get, which returns a promise. 
+*/
 const axios = require('axios');
-const connectToURL = async(url)=>{
-    const outcome = axios.get(url);
-    let listOfEntries = (await outcome).data.entries;
-    listOfEntries.forEach((entry)=>{
-      console.log(entry.Category);
+async function connectToURL(url){
+    const resp = await axios.get(url);
+    let listOfEntries = resp.data.entries;
+    let Categories = listOfEntries.map((entry)=>{
+          return entry.Category
+    });
+    Categories = [...new Set(Categories)];
+    Categories.forEach(async (Category)=>{
+      if (Category.startsWith("A")) {
+              try {
+                const resp = await axios({
+                  method: 'get',
+                  url: "https://api.publicapis.org/entries?Category="+Category,
+                  responseType: 'json'
+                })
+                console.log(Category+"   "+resp.data.count);
+              } 
+              catch(e) {
+                console.log(e);
+              }
+      }
     });
 }
-
-console.log("Before connect URL");
-connectToURL('https://api.publicapis.org/entries');
-console.log("After connect URL");
+connectToURL('https://api.publicapis.org/entries').catch(err => {
+    console.log(err.toString())
+});
